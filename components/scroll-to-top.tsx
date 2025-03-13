57cc84ff47330f3fce17331e7bd0b31fc4943d5e
isiagi <isiagigeofrey0@gmail.com>
2025-03-13 09:38:01 +0300

    refactor

--- components/scroll-to-top.tsx
+++ components/scroll-to-top.tsx
@@ -1,19 +1,27 @@
-"use client"
+"use client";
 
-import { useEffect } from "react"
-import { usePathname, useSearchParams } from "next/navigation"
+import { useEffect } from "react";
+import { usePathname, useSearchParams } from "next/navigation";
+import { Suspense } from "react";
 
-export default function ScrollToTop() {
-  const pathname = usePathname()
-  const searchParams = useSearchParams()
+function ScrollToTopContent() {
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
 
   useEffect(() => {
     window.scrollTo({
       top: 0,
       behavior: "smooth",
-    })
-  }, [pathname, searchParams])
+    });
+  }, [pathname, searchParams]);
 
-  return null
+  return null;
 }
 
+export default function ScrollToTop() {
+  return (
+    <Suspense fallback={null}>
+      <ScrollToTopContent />
+    </Suspense>
+  );
+}
